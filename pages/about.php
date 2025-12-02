793d5dcca0025889521ce5a96c504ba62789c9a2
euroscot & Air Scotland Virtual <webmaster@euroscot-virtual.co.uk>
2025-12-02 19:37:36 +0000

    First upload
    CS

---
+++ pages/about.php
@@ -0,0 +1,20 @@
+<div class="page-wrapper">
+  <div class="main-container">
+    <section class="main-content section-wrapper">
+      <!-- Your About Us / dropdown content goes here -->
+      <h1>About Us</h1>
+      <?php include(__DIR__ . '/../includes/submenu-about.php'); ?>
+      <div class="menu-image">
+        <img src="/assets/images/aboutimage.jpg" alt="About EUS">
+      </div>
+      <section class="content-area">
+        <!-- all your dropdowns and text -->
+      </section>
+    </section>
+
+    <aside class="eus-home-right-column">
+      <h1 class="eus-home-column-title">Active Flights</h1>
+      <div class="eus-home-active-flights" id="flightStatus"></div>
+    </aside>
+  </div>
+</div>
